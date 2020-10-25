35a02ec2fc24cbe8eb567addaa8bbf44774e9fd6
Nate Korous <development.nk@gmail.com>
2020-10-25 17:44:17 -0600

    updated code so that it the inputs console.log correctly

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-let taskID="0";
-let taskDesc="1";
-let theDate="2";
-
-document.getElementById("taskID").innerHTML = taskID;
-document.getElementById("taskDesc").innerHTML = taskDesc;
-document.getElementById("theDate").innerHTML = theDate;
-
-function submit(){
-    console.log(taskID);
-    console.log(taskDesc);
-    console.log(theDate);
+function submit(taskName, taskDescription, taskDate) {
+    // As you can see i'm putting all the console.logs in the same object here
+    // and since both the object and the properties name are the same i only have
+    // to specify them ones. 
+    console.log({ taskName, taskDescription, taskDate })
 }
